8c783532f3e0e5615e8f9273d15b580738b430b5
arturoluna <art@arturoluna.com>
2018-02-27 09:51:42 -0800

    updates to posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,26 @@
-
-
-
 $(document).ready(function() {
         // Transition effect for navbar
+        // $('.navbar-toggler').change(function(){
+        //   $(this).attr("aria-expanded");
+        //   console.log($(this).attr("aria-expanded"));
+        // });
+        $('#collapseDiv').on('shown.bs.collapse', function () {
+          $('.navbar').addClass('solid');
+         console.log("Opened")
+      });
+        // var isExpanded = $('.navbar-toggler').
         $(window).scroll(function() {
           // checks if window is scrolled more than 500px, adds/removes solid class
+
           if($(this).scrollTop() > 20) {
+
               $('.navbar').addClass('solid');
-              console.log("main.js");
+
           } else {
+
               $('.navbar').removeClass('solid');
           }
+          // console.log(isExpanded);
         });
         // Scroll To
         $('a[href*="#"]').on('click', function (e) {
@@ -23,6 +33,36 @@ $(document).ready(function() {
 
         $("body").tooltip({ selector: '[data-toggle=tooltip]' });
 
+
+
+        /* resize the image(s) on page load */
+      	$(document).ready(function() {
+      		resize_all_parallax();
+      	});
+
+      	/* resize the image(s) on page resize */
+      	$(window).on('resize', function(){
+      		resize_all_parallax();
+      	});
+
+      	/* keep all of your resize function calls in one place so you don't have to edit them twice (on page load and resize) */
+      	function resize_all_parallax() {
+      		var div_id = 'cover'; /* the ID of the div that you're resizing */
+      		var img_w = 1000; /* the width of your image, in pixels */
+      		var img_h = 864; /* the height of your image, in pixels */
+      		resize_parallax(div_id,img_w,img_h);
+      	}
+
+      	/* this resizes the parallax image down to an appropriate size for the viewport */
+      	function resize_parallax(div_id,img_w,img_h) {
+      		var div = $('#' + div_id);
+      		var divwidth = div.width();
+      		if (divwidth < 769) { var pct = (img_h/img_w) * 105; } /* show full image, plus a little padding, if on static mobile view */
+      		else { var pct = 60; } /* this is the HEIGHT as percentage of the current div WIDTH. you can change it to show more (or less) of your image */
+      		var newheight = Math.round(divwidth * (pct/100));
+      		newheight = newheight  + 'px';
+      		div.height(newheight);
+      	}
 
 
 });
